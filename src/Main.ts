a7905a61d024139ca3de6caae37b066c624b75d4
William Makley <wmakley@tortus.com>
2017-02-28 15:01:33 -0500

    use webpack

--- src/Main.ts
+++ src/Main.ts
@@ -3,5 +3,5 @@ import { Game } from './Game'
 document.addEventListener('DOMContentLoaded', function (event) {
   let canvas = <HTMLCanvasElement>document.getElementById('canvas');
   let fontSize = parseInt(canvas.dataset['fontSize']);
-  window['Game'] = new Game(canvas, fontSize);
+  (<any>window)['Game'] = new Game(canvas, fontSize);
 });
