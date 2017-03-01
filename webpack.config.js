5efb2c010425b74ea64d2cf307ccc2e67c8ce445
William Makley <wmakley@tortus.com>
2017-03-01 14:11:43 -0500

    add .editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 var path = require("path");
 
 module.exports = {
-    entry: "./src/Main.ts",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
+  entry: "./src/Main.ts",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' as resolvable extension.
+    extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+  },
+
+  module: {
+    loaders: [
+    // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+    { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+    ]
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, "static"),
+    port: 9000,
+    publicPath: "/dist/",
+    overlay: {
+      warnings: true,
+      errors: true
     },
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
-    resolve: {
-        // Add '.ts' as resolvable extension.
-        extensions: [".webpack.js", ".web.js", ".ts", ".js"]
-    },
-
-    module: {
-        loaders: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-        ]
-    },
-
-    devServer: {
-      contentBase: path.join(__dirname, "static"),
-      port: 9000,
-      publicPath: "/dist/",
-      overlay: {
-        warnings: true,
-        errors: true
-      },
-      watchContentBase: true
-    }
+    watchContentBase: true
+  }
 };
